4354fc3290bdb849459bc095d5072937b9144e6e
Yuki Fujiwara <sky.y.0079@gmail.com>
2017-06-21 04:46:18 +0900

    Initial commit

---
+++ function.js
@@ -0,0 +1,14 @@
+// ここに関数(function)を書く
+// 注意：同じ名前の関数は、下に書いたものが優先される
+
+// 以下は記入例。
+// コメントに「どこに書いてあるコードか」が分かるように書く。（最低でもページ数を書く）
+
+// リスト4.1 (p.79)
+function sum() {
+    var total = 0;
+    for (var counter = 1; counter <= 10; counter++ ){
+        total += counter;
+    }
+    window.alert(total);
+}
